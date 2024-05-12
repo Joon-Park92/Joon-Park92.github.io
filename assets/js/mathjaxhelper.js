29954c8842a044f1d531eaa0ab3b45227cbea5a0
Joon-Park92 <Joon-Park92@users.noreply.github.com>
2024-05-12 13:53:19 +0000

    deploy: 4252a3c10ae3a45ef744eeeb3edab90f46a5b9d5

--- assets/js/mathjaxhelper.js
+++ assets/js/mathjaxhelper.js
@@ -1,21 +1,32 @@
 // https://squidfunk.github.io/mkdocs-material/reference/math/#mathjax-mkdocsyml
 
-window.MathJax = {
-  tex: {
-    inlineMath: [['$','$']],
-    displayMath: [['$$', '$$']],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  }
-};
+// window.MathJax = {
+//   tex: {
+//     inlineMath: [['$','$']],
+//     displayMath: [['$$', '$$']],
+//     processEscapes: true,
+//     processEnvironments: true
+//   },
+//   options: {
+//     ignoreHtmlClass: ".*|",
+//     processHtmlClass: "arithmatex"
+//   }
+// };
 
-document$.subscribe(() => { // (1)!
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
+// document$.subscribe(() => { // (1)!
+//   MathJax.startup.output.clearCache()
+//   MathJax.typesetClear()
+//   MathJax.texReset()
+//   MathJax.typesetPromise()
+// })
+
+document$.subscribe(({ body }) => {
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$",  right: "$$",  display: true },
+      { left: "$",   right: "$",   display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ],
+  })
 })
